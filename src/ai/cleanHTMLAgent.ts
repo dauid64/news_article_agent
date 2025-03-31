a08389a1221645d1f13bfaaae1e952c691d7fc82
dauid64 <carlosdauid64br@gmail.com>
2025-03-31 12:11:56 -0300

    structed output in CleanHTMLAgent

--- src/ai/cleanHTMLAgent.ts
+++ src/ai/cleanHTMLAgent.ts
@@ -2,11 +2,13 @@ import logger from "../logger";
 import OpenAI from "openai";
 import { Article } from "../models/articles";
 import * as cheerio from 'cheerio';
+import { ResponseTextConfig } from "openai/resources/responses/responses";
 
 export class CleanHTMLAgent{
     readonly client: OpenAI;
     readonly qdrantClient: any;
     readonly systemPrompt: string;
+    readonly text: ResponseTextConfig;
 
     constructor() {
         logger.debug("Agent initialized");
@@ -16,17 +18,45 @@ export class CleanHTMLAgent{
         });
 
         this.systemPrompt = `
-            You are a assistant specialized in extracting data from HTML, your task is to extract the data and return it in JSON format with the following data and format which will be between three quotes.
-
-            """
-                {
-                    "title": "Title of the Article",
-                    "content": "Content of the Article",
-                    "url": "URL of the Article",
-                    "datePublished": "Published date of the Article",
-                }
-            """
+            You are a assistant specialized extract data from articles that are in HTML format.
         `
+
+        this.text = {
+            format: {
+                "type": "json_schema",
+                "name": "article",
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "article": {
+                            "type": "object",
+                            "properties": {
+                                "title": {
+                                    "type": "string",
+                                    "description": "Title of the article.",
+                                },
+                                "content": {
+                                    "type": "string",
+                                    "description": "Content of the article.",
+                                },
+                                "url": {
+                                    "type": "string",
+                                    "description": "URL of the article.",
+                                },
+                                "datePublished": {
+                                    "type": "string",
+                                    "description": "Date published of the article.",
+                                }
+                            },
+                            "required": ["title", "content", "url", "datePublished"],
+                            "additionalProperties": false
+                        }
+                    },
+                    "required": ["article"],
+                    "additionalProperties": false // <- adicionado aqui
+                }
+            }
+        }
     }
 
     async extractDataFromHTML(htmlContent: string): Promise<Article> {
@@ -39,16 +69,11 @@ export class CleanHTMLAgent{
             model: 'gpt-4o',
             instructions: this.systemPrompt,
             input: `${cleanHtml}`,
+            text: this.text,
         });
 
         const dataResponse = response.output_text;
-        const match = dataResponse.match(/```json\s*([\s\S]*?)\s*```/);
-        if (match && match[1]) {
-            const jsonString = match[1];
-            const article: Article = JSON.parse(jsonString);
-            return article
-        } else {
-            throw new Error('No JSON data found in the response');
-        }
+        
+        return JSON.parse(dataResponse) as Article;
     }
 }
